841910ccef96ef89696c77062ef27635c242dc41
kevinarchambeau <xxerexx@gmail.com>
2024-12-26 20:50:14 -0800

    updated server with repl

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -10,6 +11,7 @@ import (
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	gamelogic.PrintServerHelp()
 	connectionString := "amqp://guest:guest@localhost:5672/"
 	mqConn, err := amqp.Dial(connectionString)
 	if err != nil {
@@ -29,14 +31,45 @@ func main() {
 		}
 	}(mqChan)
 
-	testMessage := routing.PlayingState{
+	serverMessage := routing.PlayingState{
 		IsPaused: true,
 	}
 
-	err = pubsub.PublishJSON(mqChan, routing.ExchangePerilDirect, routing.PauseKey, testMessage)
-	if err != nil {
-		log.Fatal("Failed to publish message", err)
+	for {
+		command := gamelogic.GetInput()
+		switch command[0] {
+		case "pause":
+
+			fmt.Println("pausing")
+			serverMessage.IsPaused = true
+			err = pubsub.PublishJSON(mqChan, routing.ExchangePerilDirect, routing.PauseKey, serverMessage)
+			if err != nil {
+				log.Fatal("Failed to publish message", err)
+			}
+		case "resume":
+			fmt.Println("resuming")
+			serverMessage.IsPaused = false
+			err = pubsub.PublishJSON(mqChan, routing.ExchangePerilDirect, routing.PauseKey, serverMessage)
+			if err != nil {
+				log.Fatal("Failed to publish message", err)
+			}
+		case "quit":
+			goto loopEnd
+		default:
+			fmt.Println("unknown command")
+		}
 	}
+loopEnd:
+	fmt.Println("Goodbye!")
+
+	//testMessage := routing.PlayingState{
+	//	IsPaused: true,
+	//}
+
+	//err = pubsub.PublishJSON(mqChan, routing.ExchangePerilDirect, routing.PauseKey, testMessage)
+	//if err != nil {
+	//	log.Fatal("Failed to publish message", err)
+	//}
 
 	//signalChan := make(chan os.Signal, 1)
 	//signal.Notify(signalChan, os.Interrupt)
